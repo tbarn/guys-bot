45d72a3449611afa89a8fe8fbed63f1f2845ac68
Taylor Barnett <tbarn@users.noreply.github.com>
2017-11-09 20:00:04 -0600

    fix event type typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const handleGuys = require('./lib/handle-guys')
 function probotPlugin (robot) {
   robot.on([
     'issue_comment.created',
-    'issue.opened',
+    'issues.opened',
     'pull_request.opened'
   ], handleGuys.bind(null, robot))
 }
